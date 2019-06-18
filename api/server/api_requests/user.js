c43930b7b6f4dff6ab545cfadb6e026f0d03ee5f
Debrej <t.bellanger34@gmail.com>
2019-06-18 17:23:59 +0200

    feat(api): add /user request
    example request to set the conventions

--- api/server/api_requests/user.js
+++ api/server/api_requests/user.js
@@ -1,10 +1,29 @@
 module.exports = function(app, connection){
 
+    const sql_requests = require('./sql_requests/user.json');
+
     console.log('user requests loaded');
 
+
+    /*
+        Retrieve all users in the database, with all the fields
+     */
     app.get('/user', function(req, res){
+
+        // Logging the request
         console.log(req.method + ' ' + req.originalUrl);
-        res.send({'method': req.method, 'originalUrl': req.originalUrl, 'baseUrl': req.baseUrl});
+
+        // SQL request
+        connection.query(sql_requests.get_all_users, function(err, rows, fields){
+
+            //if an error occurs, send status 1 with the error message
+            if (err) {res.send({'status': 1, 'SQL_message': err});}
+
+            //else, send status 0 with the results of the request
+            else{
+                res.send({'status': 0, 'user' : rows});
+            }
+        });
     });
 
     app.post('/user', function(req, res){
