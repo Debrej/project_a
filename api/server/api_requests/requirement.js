41cd66f152ea4896a44846935236ff5e14a0838a
lcgiry <lc.giry@gmail.com>
2019-10-05 15:34:15 +0200

    feat(api): requirement CRUD

--- api/server/api_requests/requirement.js
+++ api/server/api_requests/requirement.js
@@ -1,20 +1,117 @@
-module.exports = function(app, connection){
+module.exports = function(app, sequelize, models){
 
     console.log('requirement requests loaded');
 
+    let Requirement = models.Requirement;
+    /**
+     * This request gets all the requirements in the database.
+     * arguments :
+     *              none
+     *
+     */
     app.get('/requirement', function(req, res){
-        res.send({'method': req.method, 'originalUrl': req.originalUrl, 'baseUrl': req.baseUrl});
+        Requirement.findAll()
+            .then(requirements => {
+                res.send({'requirements': requirements});
+            })
+            .catch(err =>{
+                res.send({'error': err})
+            });
     });
 
-    app.post('/requirement', function(req, res){
-        res.send({'method': req.method, 'originalUrl': req.originalUrl, 'baseUrl': req.baseUrl});
+    /**
+     *   This request gets an requirement according to its id.
+     *   arguments :
+     *               id : the id of the requirement
+     *   returns :
+     *               a json object containing the requirement
+     */
+
+    app.get('requirement/:id',function (req,res) {
+        Requirement.findByPk(req.params.id)
+            .then(requirement =>{
+                res.send({'requirement': requirement})
+            })
+            .catch(err =>{
+                res.send({'error': err})
+            });
     });
 
-    app.put('/requirement', function(req, res){
-        res.send({'method': req.method, 'originalUrl': req.originalUrl, 'baseUrl': req.baseUrl});
+    /**
+     * This request creates a new requirement with the quantity, userId, teamId and taskId
+     * arguments :
+     *          quantity : the number of needed volunteer(s)
+     *          userId : the Id of the needed volunteer(s)
+     *          teamId : the team of the needed volunteer(s)
+     *          taskId : the Id of the task for which the requirement is created
+     * returns :
+     *          a json array containing the created requirement(s)
+     */
+
+    app.post('/requirements', function(req, res){
+        Requirement.bulkCreate(req.body.requirements)
+            .then(requirements => {
+                res.send({'requirements': requirements})
+            })
+            .catch(err => {
+                res.send({'error': err});
+            });
     });
+
+    /**
+     *  This requests updates a requirement according to its id.
+     *  arguments :
+     *              id : the id of the shift
+     *              quantity : the number of needed volunteer(s)
+     *              userId : the Id of the needed volunteer(s)
+     *              teamId : the team of the needed volunteer(s)
+     *              taskId : the Id of the task for which the requirement is created
+     *  returns :
+     *              the updated object
+     */
+
+    app.put('/requirement/:id', function(req, res){
+        Requirement.update(
+            {
+                quantity : req.body.quantity,
+                user_id : req.body.user_id,
+                team_id : req.body.team_id,
+                task_id : req.body.task_id
+            },{
+                where : {
+                    id: req.params.id
+                }
+            }
+        ).then( () => {
+            Requirement.findByPk(req.params.id)
+                .then(requirement =>{
+                    res.send({'requirement': requirement});
+                })
+                .catch(err => {
+                    res.send({'error': err});
+                });
+        }).catch(err => {
+            res.send({'error': err})
+        });
+    });
+
+    /**
+     *  This requests deletes a requirement according to its id.
+     *  arguments :
+     *              id : the id of the requirement     *
+     *  returns :
+     *              a result being 1 if succeeded, 0 else
+     */
 
     app.delete('/requirement', function(req, res){
-        res.send({'method': req.method, 'originalUrl': req.originalUrl, 'baseUrl': req.baseUrl});
+        Requirement.destroy({
+            where:{
+                id: req.params.id
+            }
+        }).then(requirement => {
+            res.send({'result': requirement})
+        }).catch(err =>{
+            res.send({'error': err})
+        });
     });
 };
