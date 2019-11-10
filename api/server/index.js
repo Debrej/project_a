92d126181c85f9305fb788984f9f43681389d845
Debrej <t.bellanger34@gmail.com>
2019-11-10 18:17:35 +0100

    feat(api): adding CORS support

--- api/server/index.js
+++ api/server/index.js
@@ -46,6 +46,15 @@ app.use(function(req, res, next){
     next();
 });
 
+/*
+*   This method will allow CORS
+*/
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:2445"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //endregion
 
 //region SERVER START
