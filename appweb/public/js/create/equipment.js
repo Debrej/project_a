2b3e3da5958a7c4613611260b487020c8ff6fd71
Debrej <t.bellanger34@gmail.com>
2019-11-11 13:30:37 +0100

    feat(front): adding create equipment form
    adding titles to forms

---
+++ appweb/public/js/create/equipment.js
@@ -0,0 +1,53 @@
+const host = "http://localhost:2424";
+
+const app = new Vue({
+    el: '#app',
+    data () {
+        return {
+            value: null,
+            name: null,
+            equipment_type: null,
+            equipment_types: null,
+            locations: null,
+            pickup_location: null,
+            drop_location: null,
+            quantity: null,
+            description: null
+        }
+    },
+    components: { vSelect: VueSelect.vSelect },
+    created: function () {
+        axios.get(host+"/equipment_type")
+            .then(res => {
+                this.equipment_types = res.data.equipment_type;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        axios.get(host+"/location")
+            .then(res => {
+                this.locations = res.data.location;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    },
+    methods: {
+        submit: function () {
+            axios.post(host+'/equipment',{
+                    "name": this.name,
+                    "description": this.description,
+                    "quantity": this.quantity,
+                    "equipment_type_id": this.equipment_type.id,
+                    "pickup_location": this.pickup_location.id,
+                    "drop_location": this.drop_location.id
+                })
+                .then(res => {
+                    console.log(res.data.equipment);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+});
--- appweb/routes/create.js
+++ appweb/routes/create.js
@@ -1,12 +1,24 @@
 const express = require('express');
 let router = express.Router();
 
+router.get('/activity', (req, res) => {
+    res.render('create/activity.ejs');
+});
+
+router.get('/equipment', (req, res) => {
+    res.render('create/equipment.ejs');
+});
+
 router.get('/event', (req, res) => {
     res.render('create/event.ejs');
 });
 
 router.get('/location', (req, res) => {
     res.render('create/location.ejs');
+});
+
+router.get('/task', (req, res) => {
+    res.render('create/task.ejs');
 });
 
 module.exports = router;
