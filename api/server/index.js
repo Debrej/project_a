a16f7396f435337a7e379bcd2dc35d216d2d2903
Debrej <t.bellanger34@gmail.com>
2019-12-09 16:18:59 +0100

    feat(api): config changes

--- api/server/index.js
+++ api/server/index.js
@@ -52,7 +52,7 @@ app.use(function(req, res, next){
 *   This method will allow CORS
 */
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", config.CORS_origin); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
