dda3fdfc16f525dde39e1a6ac976087939b0b05e
Debrej <t.bellanger34@gmail.com>
2020-01-11 15:29:18 +0100

    feat(appweb): uploading files for user + refactoring form in carousel to be more readable #3

--- appweb/src/components/create/User.vue
+++ appweb/src/components/create/User.vue
@@ -6,186 +6,195 @@
           <h1>Create a user</h1>
         </v-row>
         <v-form v-model="valid" ref="create_user">
-          <v-divider></v-divider>
-          <!-- Name and surname -->
-          <v-row justify="center">
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model="user.first_name"
-                :rules="nameRules"
-                :counter="255"
-                label="First name"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model="user.surname"
-                :counter="255"
-                label="Surname"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                v-model="user.last_name"
-                :rules="nameRules"
-                :counter="255"
-                label="Last name"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <!-- Phone and email -->
-          <v-row justify="center">
-            <v-col cols="12" md="4">
-              <v-text-field
-                v-model="user.phone_number"
-                :rules="phoneRules"
-                label="Phone"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field
-                v-model="user.email"
-                :rules="emailRules"
-                label="Email"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <!-- Birthday -->
-          <v-row justify="center">
-            <v-col cols="12" md="4">
-              <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on }">
+          <v-carousel light hide-delimiter-background v-model="pagination">
+            <v-carousel-item>
+              <!-- Name and surname -->
+              <v-row justify="center">
+                <v-col cols="12" md="3">
                   <v-text-field
-                    v-model="user.birthday"
-                    label="Birthday"
-                    :rules="birthdayRules"
-                    readonly
+                    v-model="user.first_name"
+                    :rules="nameRules"
+                    :counter="255"
+                    label="First name"
                     required
-                    v-on="on"
                   ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="user.birthday"
-                  @input="menu = false"
-                  :max="new Date().toISOString()"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-          <!-- Licence date and file -->
-          <v-row justify="center">
-            <v-col cols="12" md="3">
-              <v-checkbox v-model="user.licence" label="Licence ?"></v-checkbox>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-menu
-                v-model="menu2"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                :disabled="!user.licence"
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on }">
+                </v-col>
+                <v-col cols="12" md="3">
                   <v-text-field
-                    v-model="user.licence_date"
-                    label="Licence Date"
-                    :disabled="!user.licence"
-                    readonly
-                    v-on="on"
+                    v-model="user.surname"
+                    :counter="255"
+                    label="Surname"
+                    required
                   ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="user.licence_date"
-                  @input="menu = false"
-                  :max="new Date().toISOString()"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-file-input
-                :disabled="!user.licence"
-                accept=".pdf"
-                label="Licence pdf scan file"
-                v-model="user.licence_scan"
-              ></v-file-input>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-          <!-- Specialty, t-shirt size and profile pic url -->
-          <v-row justify="center">
-            <v-col cols="12" md="3">
-              <v-select
-                :items="specialties"
-                v-model="user.specialty_id"
-                item-text="specialtyName"
-                item-value="id"
-                label="Specialty"
-              ></v-select>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-select
-                :items="tshirt_sizes"
-                v-model="user.tshirt_size"
-                label="T-Shirt Size"
-              ></v-select>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-file-input
-                accept=".png, .jpg"
-                label="Profile picture (png, jpg)"
-                v-model="user.profile_pic"
-              ></v-file-input>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-          <!-- Comment, experience and incapacity -->
-          <v-row justify="center">
-            <v-col cols="12" md="3">
-              <v-textarea
-                label="Comment"
-                v-model="user.comment"
-                auto-grow
-                :counter="255"
-                :rules="textRules"
-                hint="Got something to say ?"
-              ></v-textarea>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-textarea
-                label="Experience"
-                v-model="user.experience"
-                auto-grow
-                :counter="255"
-                :rules="textRules"
-                hint="Ever did anything interesting ?"
-              ></v-textarea>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-textarea
-                label="Incapacity"
-                v-model="user.incapacity"
-                auto-grow
-                :counter="255"
-                :rules="textRules"
-                hint="Is there something you cannot do ?"
-              ></v-textarea>
-            </v-col>
-          </v-row>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    v-model="user.last_name"
+                    :rules="nameRules"
+                    :counter="255"
+                    label="Last name"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <!-- Phone and email -->
+              <v-row justify="center">
+                <v-col cols="12" md="4">
+                  <v-text-field
+                    v-model="user.phone_number"
+                    :rules="phoneRules"
+                    label="Phone"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-text-field
+                    v-model="user.email"
+                    :rules="emailRules"
+                    label="Email"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <!-- Birthday -->
+              <v-row justify="center">
+                <v-col cols="12" md="4">
+                  <v-menu
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="user.birthday"
+                        label="Birthday"
+                        :rules="birthdayRules"
+                        readonly
+                        required
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="user.birthday"
+                      @input="menu = false"
+                      :max="new Date().toISOString()"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </v-carousel-item>
+            <v-carousel-item>
+              <!-- Licence date and file -->
+              <v-row justify="center">
+                <v-col cols="12" md="3">
+                  <v-checkbox
+                    v-model="user.licence"
+                    label="Licence ?"
+                  ></v-checkbox>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    :disabled="!user.licence"
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="user.licence_date"
+                        label="Licence Date"
+                        :disabled="!user.licence"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="user.licence_date"
+                      @input="menu = false"
+                      :max="new Date().toISOString()"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-file-input
+                    :disabled="!user.licence"
+                    accept=".pdf"
+                    label="Licence pdf scan file"
+                    v-model="user.licence_scan"
+                  ></v-file-input>
+                </v-col>
+              </v-row>
+            </v-carousel-item>
+            <v-carousel-item>
+              <!-- Specialty, t-shirt size and profile pic url -->
+              <v-row justify="center">
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="specialties"
+                    v-model="user.specialty_id"
+                    item-text="specialtyName"
+                    item-value="id"
+                    label="Specialty"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="tshirt_sizes"
+                    v-model="user.tshirt_size"
+                    label="T-Shirt Size"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-file-input
+                    accept=".png, .jpg"
+                    label="Profile picture (png, jpg)"
+                    v-model="user.profile_pic"
+                  ></v-file-input>
+                </v-col>
+              </v-row>
+            </v-carousel-item>
+            <v-carousel-item>
+              <!-- Comment, experience and incapacity -->
+              <v-row justify="center">
+                <v-col cols="12" md="3">
+                  <v-textarea
+                    label="Comment"
+                    v-model="user.comment"
+                    auto-grow
+                    :counter="255"
+                    :rules="textRules"
+                    hint="Got something to say ?"
+                  ></v-textarea>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-textarea
+                    label="Experience"
+                    v-model="user.experience"
+                    auto-grow
+                    :counter="255"
+                    :rules="textRules"
+                    hint="Ever did anything interesting ?"
+                  ></v-textarea>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-textarea
+                    label="Incapacity"
+                    v-model="user.incapacity"
+                    auto-grow
+                    :counter="255"
+                    :rules="textRules"
+                    hint="Is there something you cannot do ?"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-carousel-item>
+          </v-carousel>
           <v-divider></v-divider>
           <!-- Validate and reset form -->
           <v-row justify="center">
@@ -215,6 +224,7 @@
 export default {
   name: "User",
   data: () => ({
+    pagination: 0,
     valid: false,
     menu: false,
     menu2: false,
@@ -251,8 +261,8 @@ export default {
     birthdayRules: [v => !!v || "Birthday is required"],
     phoneRules: [
       v => !!v || "Phone number is required",
-      v => v.length === 10 || "Phone number must be 10 digits",
-      v => /^\d+$/.test(v) || "Phone number must only contain numbers"
+      v => /^\d+$/.test(v) || "Phone number must only contain numbers",
+      v => v.length === 10 || "Phone number must be 10 digits"
     ]
   }),
   created() {
@@ -271,8 +281,25 @@ export default {
         this.user.licence_scan = null;
       }
       this.$axios.post(this.$host + "user", this.user).then(res => {
-        //eslint-disable-next-line
-        console.log(res.data);
+        let user = res.data.user;
+        let promises = [];
+        if (this.user.profile_pic !== null)
+          promises.push(
+            this.$axios.put(this.$host + "user/photo/" + user.id, {
+              file: this.user.profile_pic
+            })
+          );
+        if (this.user.licence_scan !== null) {
+          promises.push(
+            this.$axios.put(this.$host + "user/licence/" + user.id, {
+              file: this.user.licence_scan
+            })
+          );
+        }
+        this.$axios.all(promises).then(() => {
+          //eslint-disable-next-line
+            console.log("i think we're done here");
+        });
       });
     },
     reset: function() {
